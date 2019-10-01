041725ae12a1dd81bc66f7314dc48ef6fd57281b
sjflaherty <sjflaherty@loyola.edu>
2019-10-01 11:19:04 -0400

    created android project and began to create some basic design examples

---
+++ app/src/main/java/com/example/sarepach/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sarepach;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public void onStart() {
+        super.onStart();
+    }
+}
