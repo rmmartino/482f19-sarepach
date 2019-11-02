f77f14635aa7ccc4edd53c0e44e59ba4ce1f0b9c
Patrick Sacchet <pjsacchet@loyola.edu>
2019-11-02 00:12:54 -0400

    Test database creation added

--- app/src/test/java/com/example/sarepach/TestDatabase.java
+++ app/src/test/java/com/example/sarepach/TestDatabase.java
@@ -1,6 +1,9 @@
 package com.example.sarepach;
 
 import org.junit.Assert.*;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Test class for Database class
@@ -9,6 +12,15 @@ import org.junit.Assert.*;
  */
 
 public class TestDatabase {
+
+    @Test
+    public void TestDatabaseCreation() {
+        Database db = new Database("cs-database.cs.loyola.edu", "pjsacchet", "1737480");
+        assertEquals(db.Hostname, "cs-database.cs.loyola.edu");
+        assertEquals(db.Username, "pjsacchet");
+        assertEquals(db.Password, "1737480");
+    }
+
 
 
 
