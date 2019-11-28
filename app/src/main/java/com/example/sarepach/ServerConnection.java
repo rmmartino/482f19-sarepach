9ec0cf988b5601cc4eb13d220bf75bd3756701b0
Patrick Sacchet <pjsacchet@loyola.edu>
2019-11-28 00:24:56 -0500

    Very base outline for server connection class added

---
+++ app/src/main/java/com/example/sarepach/ServerConnection.java
@@ -0,0 +1,16 @@
+/**
+ * @author Patrick Sacchet
+ * @version 1.0
+*/
+
+package com.example.sarepach;
+
+import java.util.*;
+
+/**
+ * Class will represent a link to the studentvhost server
+*/
+public class ServerConnection {
+
+  
+}
