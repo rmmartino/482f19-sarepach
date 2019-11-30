19acf595a57c918df1056fd7db833e68852ebb3b
Patrick Sacchet <pjsacchet@loyola.edu>
2019-11-30 16:25:30 -0500

    Updated functionality and layout of ServerConnection

--- app/src/main/java/com/example/sarepach/ServerConnection.java
+++ app/src/main/java/com/example/sarepach/ServerConnection.java
@@ -2,15 +2,12 @@
  * @author Patrick Sacchet
  * @version 1.0
 */
-
+// Package dependency
 package com.example.sarepach;
 
-import java.net.MalformedURLException;
+// Dependencies for socket protocol and logging
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.*;
 import android.util.Log;
-
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -19,25 +16,51 @@ import java.net.Socket;
 */
 public class ServerConnection {
 
-    public int EstablishConnection(String baseaddress, int port) {
+    /**
+     * Each ServerConnection will have the baseaddress for our studentvhost2 server
+     */
+    protected final String BaseAddress = "http://sarepach.cs.loyola.edu";
+
+    /**
+     * Each ServerConnection will have the port the server is hosted on
+     */
+    protected final int Port = 80;
+
+    /**
+     * Method will get the current ip address of the base name server
+     * @return ip - InetAddress object with which we can establish a connection
+     */
+    public InetAddress GetIPHost() {
+        InetAddress ip = null;
         try {
-            InetAddress addr;
-            InetAddress ip = InetAddress.getByName(new URL("http://sarepach.cs.loyola.edu").getHost());
-            //Socket sock = new Socket(baseaddress, port);
-            Socket sock = new Socket(baseaddress, port);
-            addr = sock.getInetAddress();
-            //System.out.println(addr);
-            Log.w("ServerConnection", "ServerConnection established" + addr);
-            sock.close();
-            return 1;
+            ip = InetAddress.getByName(new URL(this.BaseAddress).getHost());
+        } catch ( java.net.MalformedURLException | java.net.UnknownHostException e) {
+            Log.w("ServerConnection", "Port gathering failed" + e);
+            return ip;
+        }
+        return ip;
+    }
+
+    /**
+     * Method will establish a connection with a InetAddress object passed to it
+     * @return 1 on success, 0 otherwise
+     */
+    public Socket EstablishConnection() {
+        Socket sock = null;
+        try {
+            // Get the ip address via a InetAddress object
+            InetAddress ip = this.GetIPHost();
+            // Open a socket with the address and known port
+            sock = new Socket(ip.getHostAddress(), this.Port);
+            Log.w("ServerConnection", "ServerConnection established" + ip.getHostName() + ip.getHostAddress());
         } catch (java.io.IOException e) {
             Log.w("ServerConnection", "ServerConnection failed" + e);
-            //System.out.println(e);
-            return 0;
+            return sock;
         } catch(java.lang.SecurityException ee) {
             Log.w("ServerConnection", "Security Exception" + ee);
+            return sock;
         }
-    return 0;
+    return sock;
     }
 
 
