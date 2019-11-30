631cb1c2b618a7426da149ae3e40cf4d7649b5da
Patrick Sacchet <pjsacchet@loyola.edu>
2019-11-30 16:27:03 -0500

    Testing functions implemented for updated ServerConnection class

--- app/src/test/java/com/example/sarepach/TestServerConnection.java
+++ app/src/test/java/com/example/sarepach/TestServerConnection.java
@@ -1,14 +1,42 @@
+/**
+ * @author Patrick Sacchet
+ * @version 1.0
+ */
+// Package dependency
 package com.example.sarepach;
 
+// Testing dependencies
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
+// ServeConnect dependencies
+import java.net.InetAddress;
+import java.net.Socket;
+
+/**
+ * Class will serve to implement unit tests for all aspects of a ServerConnection class
+ */
 public class TestServerConnection {
 
+    /**
+     * Testing ServerConnect ability to grab ip address of our host server
+     */
+    @Test
+    public void TestGetIPHost() {
+        ServerConnection serverconnect = new ServerConnection();
+        InetAddress ip = serverconnect.GetIPHost();
+        String hostaddress = ip.getHostAddress();
+        assertEquals(hostaddress, "144.126.12.148");
+    }
+
+    /**
+     * Testing ServerConnect ability to establish a connection to our host via ip address and port confirmation
+     */
     @Test
     public void TestEstablishConnection() {
         ServerConnection serverconnect = new ServerConnection();
-        assertEquals(serverconnect.EstablishConnection("144.126.12.148", 80), 1);
+        Socket sock = serverconnect.EstablishConnection();
+        assertEquals(sock.getInetAddress().getHostAddress(), "144.126.12.148");
+        assertEquals(sock.getPort(), 80);
     }
 }
