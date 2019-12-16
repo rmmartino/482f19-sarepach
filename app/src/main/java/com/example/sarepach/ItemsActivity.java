255be75899912e5a7b34ff726411f4ce6544f1c4
Patrick Sacchet <pjsacchet@loyola.edu>
2019-12-16 14:03:45 -0500

    Adding item id through intent

--- app/src/main/java/com/example/sarepach/ItemsActivity.java
+++ app/src/main/java/com/example/sarepach/ItemsActivity.java
@@ -3,10 +3,15 @@ package com.example.sarepach;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -37,6 +42,16 @@ public class ItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+        String allItems = "";
+        try {
+            // When we open the items activity page we load all items in the database until the user wants to search / filter
+            AsyncRetrieveBids asyncRetrieveBids = new AsyncRetrieveBids();
+            allItems = asyncRetrieveBids.execute().get();
+            TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
+            addItemEntitys(tableLayout, allItems);
+        } catch(Exception e){
+            Log.w("ItemsActivity", "Error loading all items: " +  e);
+        }
 
 
 
@@ -67,9 +82,93 @@ public class ItemsActivity extends AppCompatActivity {
      * @param v
      *            the screen view
      */
-    public void goDescription(View v) {
+    public void goDescription(View v, int NewBid) {
         Intent intent = new Intent(this, DescriptionActivity.class);
+        intent.putExtra("ITEM_ID", NewBid);
         this.startActivity(intent);
+    }
+
+    /**
+     * Adds an item to a table to display the items the user as bidded on
+     * for their profile page
+     *
+     * @param v
+     *            the screen view
+     * @param item
+     *            the item being added
+     */
+    // will eventually parse through all items
+    public void addItemEntitys(TableLayout v, String item){
+        String[] items_to_display = item.split("_");
+        for(int i = 0; i < items_to_display.length; i++){
+            addTableRow(v, items_to_display[i]);
+        }
+
+
+    }
+
+    /**
+     * Adds a row to the table to display more items
+     *
+     * @param v
+     *            the screen view
+     * @param item
+     *            the item being added
+     */
+    public void addTableRow(TableLayout v, String item) {
+        try {
+            // Add table row entry to the table layout
+            TableRow tableRow = new TableRow(getApplicationContext());
+
+            //tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+            v.addView(tableRow);
+            // Add text view to the new row
+            addTextView(tableRow, item);
+            addButton(tableRow, item);
+            //addImage(tableRow, item);
+
+        }catch (Exception e) {
+            Log.w("ProfileActivityTableRow", e);
+        }
+        //addButton();
+        //ImageView imageView = new ImageView(getApplicationContext());
+        //TableLayout.LayoutParams layoutParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT);
+        //imageView.setLayoutParams(layoutParams);
+    }
+
+    /**
+     * Adds a text view to allow more items to be displayed
+     *
+     * @param v
+     *            the screen view
+     * @param item
+     *            the item being added
+     */
+    public void addTextView(TableRow v, String item){
+        TextView textView = new TextView(getApplicationContext());
+        //textView.LayoutParams layoutParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT);
+        //textView.setLayoutParams(layoutParams);
+        textView.setTextSize(24f);
+        textView.setTextColor(Color.BLACK);
+        textView.setBackgroundColor(Color.parseColor("#7EBAD1"));
+
+        textView.setText(item.split(";")[0]);
+        textView.setHeight(300);
+        textView.setWidth(700);
+        v.addView(textView);
+
+    }
+
+    public void addButton(TableRow v, String item){
+        Button button = new Button(this);
+        //button.LayoutParams layoutParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT);
+        //button.setLayoutParams(layoutParams);
+        button.setTextSize(24f);
+        button.setTextColor(Color.BLACK);
+        button.setBackgroundColor(Color.parseColor("#7EBAD1"));
+        button.setText(item.split(";")[3]);
+
+        v.addView(button);
     }
 
     /**
@@ -87,7 +186,7 @@ public class ItemsActivity extends AppCompatActivity {
         String password;
         HttpURLConnection conn;
         URL url = null;
-        private final String validateUserPHP = "http://sarepach.cs.loyola.edu/UserConnection/displayProfileBids.php";
+        private final String validateUserPHP = "http://sarepach.cs.loyola.edu/UserConnection/displayBids.php";
         public static final int CONNECTION_TIMEOUT = 10000;
         public static final int READ_TIMEOUT = 15000;
 
@@ -124,7 +223,7 @@ public class ItemsActivity extends AppCompatActivity {
         public String doInBackground(String... params) {
             try {
                 // Only testing admin code for now (will execute client code instead)
-                url = new URL(validateUserPHP + this.username);
+                url = new URL(validateUserPHP );
 
             } catch (MalformedURLException e) {
                 // TODO Auto-generated catch block
