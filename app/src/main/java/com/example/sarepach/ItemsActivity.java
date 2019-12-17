5f43148f3e5552107930e745c1b8ffb256c41d85
cnmeier <31703745+cnmeier@users.noreply.github.com>
2019-12-17 01:07:12 -0500

    filter capabilities added for categories and trending

--- app/src/main/java/com/example/sarepach/ItemsActivity.java
+++ app/src/main/java/com/example/sarepach/ItemsActivity.java
@@ -62,12 +62,12 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
         categories.add("Trending");
         categories.add("Price: Low to High");
         categories.add("Price: High to Low");
-        categories.add("Home & Kitchen");
-        categories.add("Clothing & Accessories");
-        categories.add("Food & Drink");
+        categories.add("Home and Kitchen");
+        categories.add("Clothing and Accessories");
+        categories.add("Food and Drink");
         categories.add("Technology");
-        categories.add("Travel & Vehicles");
-        categories.add("Sports & Entertainment");
+        categories.add("Travel and Vehicles");
+        categories.add("Sports and Entertainment");
 
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
@@ -243,9 +243,16 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String filterChoice = adapterView.getItemAtPosition(i).toString();
-        //Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-        if(filterChoice.equals("Trending")) {
-            Toast.makeText(adapterView.getContext(), "HI", Toast.LENGTH_LONG).show();
+        String result;
+        try {
+            // When we open the items activity page we load all items in the database until the user wants to search / filter
+            AsyncRetrieveFilter asyncRetrieveFilter = new AsyncRetrieveFilter(filterChoice);
+            result = asyncRetrieveFilter.execute().get();
+            TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
+            tableLayout.removeAllViews();
+            addItemEntitys(tableLayout, result);
+        } catch (Exception e) {
+            Log.w("ItemsActivity", "Error loading all items: " + e);
         }
     }
 
@@ -271,13 +278,11 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
 
     }
 
-
-
     /**
-     * This is a AsyncRetrieveProfileBids class that uses the Android Studio library,
+     * This is a AsyncRetrieveBids class that uses the Android Studio library,
      * AsyncTask which allows for the communication between the server and the app.
-     * This connects with the validateUser php file in the server which checks if the username
-     * and password combination of the user are correct.
+     * This connects with the displayItems php file in the server which displays all of
+     * the items on the auction items page.
      *
      * @author SaRePaCh
      * @version 1.0 12/15/2019
@@ -288,7 +293,7 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
         String password;
         HttpURLConnection conn;
         URL url = null;
-        private final String validateUserPHP = "http://sarepach.cs.loyola.edu/UserConnection/displayItems.php";
+        private final String displayItemsPHP = "http://sarepach.cs.loyola.edu/UserConnection/displayItems.php";
         public static final int CONNECTION_TIMEOUT = 10000;
         public static final int READ_TIMEOUT = 15000;
 
@@ -312,9 +317,9 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
         }
 
         /**
-         * This connects with the validateUser php file on the server to check
-         * that the email and password of the user is a correct combination in the
-         * database table
+         * This connects with the displayItems php file on the server to display
+         * all of the items in the database table including all of the information
+         * about them.
          *
          * @param params
          *
@@ -325,7 +330,7 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
         public String doInBackground(String... params) {
             try {
                 // Only testing admin code for now (will execute client code instead)
-                url = new URL(validateUserPHP );
+                url = new URL(displayItemsPHP);
 
             } catch (MalformedURLException e) {
                 // TODO Auto-generated catch block
@@ -355,7 +360,7 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
                 int response_code = conn.getResponseCode();
 
                 // Check if successful connection made
-                Log.w("ProfileActivity" , valueOf(response_code));
+                Log.w("ItemsActivity" , valueOf(response_code));
                 if (response_code == HttpURLConnection.HTTP_OK ) {
 
                     // Read data sent from server
@@ -376,7 +381,7 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
 
                 else {
 
-                    return ("unsuccessful" + validateUserPHP + this.username);
+                    return ("unsuccessful" + displayItemsPHP + this.username);
                 }
 
             } catch (IOException e) {
@@ -385,20 +390,121 @@ public class ItemsActivity extends AppCompatActivity implements AdapterView.OnIt
             } finally {
                 conn.disconnect();
             }
-
-
         }
-    /**
-     @Override
-     public void onPostExecute(String result){
-     AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-     alertDialog.setTitle("Display Result");
-     alertDialog.setMessage(result);
-     alertDialog.show();
-     }
-     */
-
 
     }
 
+    /**
+     * This is a AsyncRetrieveFilter class that uses the Android Studio library,
+     * AsyncTask which allows for the communication between the server and the app.
+     * This connects with the filterChoice php file in the server which grabs and displays
+     * all of the item that are under the specific filter the user selected in the app.
+     *
+     * @author SaRePaCh
+     * @version 1.0 12/15/2019
+     */
+    protected class AsyncRetrieveFilter extends AsyncTask<String, String, String> {
+        //ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
+        String filterChoice;
+        HttpURLConnection conn;
+        URL url = null;
+        private final String filterChoicePHP = "http://sarepach.cs.loyola.edu/UserConnection/filterChoice.php";
+        public static final int CONNECTION_TIMEOUT = 10000;
+        public static final int READ_TIMEOUT = 15000;
+
+        /**
+         * Retrieves the filter choice that the user selected
+         */
+        public AsyncRetrieveFilter(String filterChoice){
+            this.filterChoice = "?filterChoice=" + filterChoice;
+        }
+
+        /**
+         * This will interact with UI and display loading message
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //pdLoading.setMessage("\tLoading...");
+            //pdLoading.setCancelable(false);
+            //pdLoading.show();
+        }
+
+        /**
+         * This connects with the filterChoice php file on the server to display
+         * all of the items in the specific category the user selected or ranked
+         * in price depending on the order the user selected.
+         *
+         * @param params
+         *
+         * @return the output from the php file if it connects successfully, "unsuccessful" if
+         * it doesn't connect successfully
+         */
+        @Override
+        public String doInBackground(String... params) {
+            try {
+                // Only testing admin code for now (will execute client code instead)
+                url = new URL(filterChoicePHP + this.filterChoice);
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+
+                // Setup HttpURLConnection class to send and receive data from php
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("GET");
+
+                // setDoOutput to true as we receive data from json file
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                Log.w("ItemsActivity" , valueOf(response_code));
+                if (response_code == HttpURLConnection.HTTP_OK ) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                }
+
+
+                else {
+
+                    return ("unsuccessful" + filterChoicePHP + this.filterChoice);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            } finally {
+                conn.disconnect();
+            }
+        }
+    }
 }
